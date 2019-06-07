ed95e9a9cafaf6f9093c1673297f943e5a2f1f99
Johan Godfried <dajomas+github@gmail.com>
2019-06-07 10:34:36 +0200

    User specific configuration file
    Save all the user specific settings in a separate file so the user only needs to make changes to the config.h file, minimizing the change for accidental typos

---
+++ config.h
@@ -0,0 +1,15 @@
+// Replace all the placeholders (starting with "<" and ending in ">") with the correct values
+
+// Neopixel
+#define PIN            <SET YOUR DATA PIN HERE>
+#define NUMPIXELS      <SET THE NUMBER OF LIGHTS HERE>
+
+// Wifi
+char* SSID = "<SSID>";
+char* WiFiPassword = "<SSID PASSWORD>";
+char* WiFiHostname = "<HOSTNAME>";
+
+// MQTT
+const char* mqtt_server = "<MQTT SERVER IP ADDRESS>";
+const char* mqttUser = "<MQTT USER>";
+const char* mqttPass = "<MQTT PASSWORD>";
